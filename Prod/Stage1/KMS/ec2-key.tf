d3d0747a636eaf6a42ee1c57dce6fa7910361021
rootacces77 <rootacces77@gmail.com>
2025-11-28 15:39:49 +0100

    Workloads Fix

--- Prod/Stage1/KMS/ec2-key.tf
+++ Prod/Stage1/KMS/ec2-key.tf
@@ -1,4 +1,4 @@
-resource "tls_private_key" "ssh_key" {
+/*resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
@@ -6,4 +6,4 @@ resource "tls_private_key" "ssh_key" {
 resource "aws_key_pair" "ec2_key" {
   key_name   = "ec2-key"
   public_key = tls_private_key.ssh_key.public_key_openssh
-}
+}*/
