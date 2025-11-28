66a67a112b0d590f574aebe318565ab92c7fabca
rootacces77 <rootacces77@gmail.com>
2025-11-28 09:13:57 +0100

    Image Builder DB Fix2

--- Prod/Stage1/EC2-IMAGE/main.tf
+++ Prod/Stage1/EC2-IMAGE/main.tf
@@ -9,19 +9,15 @@ resource "aws_imagebuilder_image_pipeline" "web_ami_pipeline" {
 }
 
 
+resource "aws_imagebuilder_image" "web_image" {
+  image_recipe_arn                 = aws_imagebuilder_image_recipe.web_recipe.arn
+  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.web_infra.arn
 
-resource "null_resource" "run_imagebuilder_once_web" {
-  triggers = {
-    pipeline_arn = aws_imagebuilder_image_pipeline.web_ami_pipeline.arn
-  }
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      aws imagebuilder start-image-pipeline-execution \
-        --image-pipeline-arn ${aws_imagebuilder_image_pipeline.web_ami_pipeline.arn}
-    EOT
+  tags = {
+    Name = "webserver-rhel9-ansible"
   }
 }
+
 
 #DATABASE IMAGE BUILDER PIPELINE
 resource "aws_imagebuilder_image_pipeline" "db_ami_pipeline" {
