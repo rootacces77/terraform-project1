21f435950daade5599402daec84973030ec9a80b
rootacces77 <rootacces77@gmail.com>
2025-12-05 13:29:32 +0100

    Fixed Parameter

--- Management/Stage1/Organization/variable.tf
+++ Management/Stage1/Organization/variable.tf
@@ -28,7 +28,7 @@ locals {
   prod_account_id    = try(local.accounts_by_name["Prod"],null)
   network_account_id = try(local.accounts_by_name["Network"], null)
   security_account_id = try(local.accounts_by_name["Security"], null)
-  management_account_id = try(local.accounts_by_name["Management"], null)
+  management_account_id = try(local.accounts_by_name["project.practice77"], null)
 }
 
 variable "domain_name" {
