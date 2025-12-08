2d5061bdd027b03c9057ad2beace16b649cad9a3
rootacces77 <rootacces77@gmail.com>
2025-12-08 16:09:48 +0100

    Stage1 FIX

--- Network/Stage1/VPC-APP/main.tf
+++ Network/Stage1/VPC-APP/main.tf
@@ -138,7 +138,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.prod_app_public[0].id
+  subnet_id     = aws_subnet.prod_app_public["us-east-1a"].id
 
   tags = {
     Name = "nat-gw-public-1"
