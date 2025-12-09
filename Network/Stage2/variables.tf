ef11394dd6d41ceed782ad8b7343a0575de6141c
rootacces77 <rootacces77@gmail.com>
2025-12-09 13:10:07 +0100

    Fixing Stage2

--- Network/Stage2/variables.tf
+++ Network/Stage2/variables.tf
@@ -11,7 +11,7 @@ data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
     bucket = "tf-state-project-practice77"
-    key    = "accounts/management/terraform.tfstate"
+    key    = "accounts/network/terraform.tfstate"
     region = "us-east-1"
   }
 }
