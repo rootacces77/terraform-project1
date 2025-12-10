47da2708e54a0ebc03b8526f4daf9904de2c7f83
rootacces77 <rootacces77@gmail.com>
2025-12-10 17:37:19 +0100

    Fixing Templates

--- Prod/Stage1/ALB-APP/TargetGroup.tf
+++ Prod/Stage1/ALB-APP/TargetGroup.tf
@@ -6,7 +6,7 @@ resource "aws_lb_target_group" "tg" {
   vpc_id      = var.vpc_id
 
   health_check {
-    path                = "/"
+    path                = "/health"
     protocol            = "HTTP"
     matcher             = "200"
     interval            = 30
