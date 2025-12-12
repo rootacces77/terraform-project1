df57e2bc29e6b78605bd4889c77249e85ff95003
rootacces77 <rootacces77@gmail.com>
2025-12-12 17:54:19 +0100

    Stage2 ASG test

--- Prod/Stage2/main.tf
+++ Prod/Stage2/main.tf
@@ -1,7 +1,7 @@
 module "asg" {
   source = "./ASG-APP"
 
-  app_private_subnet_ids = data.terraform_remote_state.network.outputs.prod_app_private_subnets_ids
-  asg_target_group_arn   = data.terraform_remote_state.prod.outputs.target_group_id
-  app_template_id        = data.terraform_remote_state.prod.outputs.app_ec2_template_id
+  app_private_subnet_ids = local.app_private_subnet_ids
+  asg_target_group_arn   = local.asg_target_group_arn
+  app_template_id        = local.app_template_id
 } 
--- Prod/Stage2/variables.tf
+++ Prod/Stage2/variables.tf
@@ -27,6 +27,9 @@ data "terraform_remote_state" "network" {
 
 locals {
   prod_account_id = data.terraform_remote_state.management.outputs.prod_account_id
+  app_private_subnet_ids = data.terraform_remote_state.network.outputs.prod_app_private_subnets_ids
+  asg_target_group_arn   = data.terraform_remote_state.prod.outputs.target_group_id
+  app_template_id        = data.terraform_remote_state.prod.outputs.app_ec2_template_id
 }
 
 
