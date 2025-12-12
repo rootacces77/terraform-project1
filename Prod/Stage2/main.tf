a77aec33e016654f89aa10255b9d58a429837e91
rootacces77 <rootacces77@gmail.com>
2025-12-12 18:34:55 +0100

    Testing Stage2

--- Prod/Stage2/main.tf
+++ Prod/Stage2/main.tf
@@ -1,7 +1,7 @@
-/*module "asg" {
+module "asg" {
   source = "./ASG-APP"
 
   app_private_subnet_ids = local.app_private_subnet_ids
   asg_target_group_arn   = local.asg_target_group_arn
   app_template_id        = local.app_template_id
-} */
+} 
